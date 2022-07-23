9d18ff84b10e64659a485dba9c0fc40305622d04
OldRabbit736 <sylvan0212@gmail.com>
2022-07-23 14:54:45 +0900

    개인적으로 정말 궁금했던 점을 추가하는 중

--- src/test/java/com/example/jacksontutorialsbaeldung/objectMapper/MapperTests.java
+++ src/test/java/com/example/jacksontutorialsbaeldung/objectMapper/MapperTests.java
@@ -2,20 +2,24 @@ package com.example.jacksontutorialsbaeldung.objectMapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import lombok.Data;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class MapperTests {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    String getFileLocation(String filename) {
+        return "src/test/resources/target/" + filename;
+    }
 
     @Test
     @Order(1)
@@ -32,10 +36,53 @@ public class MapperTests {
         //Car car = objectMapper.readValue(json, Car.class);
 
         // 방법 2 : 파일로부터 값을 읽는다.
-        Car car = objectMapper.readValue(new File("src/test/resources/target/car2.json"), Car.class);
+        Car car = objectMapper.readValue(new File(getFileLocation("car2.json")), Car.class);
 
         assertEquals("Black", car.getColor());
         assertEquals("BMW", car.getType());
+    }
+
+    @DisplayName("Person 테스트")
+    @Test
+    void personToObject() throws IOException {
+        Person person = objectMapper.readValue(new File(getFileLocation("person1.json")), Person.class);
+
+        assertEquals(36, person.getAge());
+        assertEquals("JS", person.getName());
+    }
+
+    @DisplayName("만약 숫자 필드를 아예 보내지 않는다면?")
+    @Test
+    void personNoIntField() throws IOException {
+        Person person = objectMapper.readValue(new File(getFileLocation("personWithNoAge.json")), Person.class);
+
+        System.out.println(person);
+
+        assertEquals("JS", person.getName());
+        assertEquals(0, person.getAge());   // 0으로 초기화 시켜버린다.
+    }
+
+    @DisplayName("만약 숫자 필드를 빈 값으로 한다면?")
+    @Test
+    void personBlankIntField() throws IOException {
+        Person person = objectMapper.readValue(new File(getFileLocation("personWithBlankAge.json")), Person.class);
+
+        System.out.println(person);
+
+        assertEquals("JS", person.getName());
+        assertEquals(0, person.getAge());   // 0으로 초기화 시켜버린다.
+    }
+
+    @DisplayName("만약 숫자 필드에 숫자가 아닌 글자를 넣는다면?")
+    @Test
+    void personLetterIntField() throws IOException {
+        // objectMapper.readValue(new File(getFileLocation("personWithLetterIntField.json")), Person.class);
+        // --> Cannot deserialize value of type `int` from String "i don't know": not a valid `int` value
+
+        assertThrows(
+                InvalidFormatException.class,
+                () -> objectMapper.readValue(new File(getFileLocation("personWithLetterIntField.json")), Person.class)
+        );
     }
 }
 
---
+++ src/test/java/com/example/jacksontutorialsbaeldung/objectMapper/Person.java
@@ -0,0 +1,17 @@
+package com.example.jacksontutorialsbaeldung.objectMapper;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Person {
+
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
