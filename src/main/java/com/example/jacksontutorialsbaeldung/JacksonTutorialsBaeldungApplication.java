f0e49fcb98ed8585b59d7571f869f278e5ef8cc2
OldRabbit736 <sylvan0212@gmail.com>
2022-07-23 13:06:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/jacksontutorialsbaeldung/JacksonTutorialsBaeldungApplication.java
@@ -0,0 +1,13 @@
+package com.example.jacksontutorialsbaeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonTutorialsBaeldungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonTutorialsBaeldungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jacksontutorialsbaeldung/JacksonTutorialsBaeldungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacksontutorialsbaeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonTutorialsBaeldungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
